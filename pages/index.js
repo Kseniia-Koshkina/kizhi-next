b72656ec7ebfbc587db5b223c8ab9578a17b32fd
Koshkina Kseniia <ksusakoskina0@gmail.com>
2023-06-19 12:37:25 +0300

    -

---
+++ pages/index.js
@@ -0,0 +1,15 @@
+import Head from 'next/head'
+
+
+export default function Home() {
+  return (
+    <>
+    <Head>
+        <title>Музей-Заповедник Кижи</title>
+    </Head>
+    <h1>
+      Hello
+    </h1>
+    </>
+  )
+}
