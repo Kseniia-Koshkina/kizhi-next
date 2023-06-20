99493ef0fbcc2be3f632507d4bfdce9148465297
vasilievi <vasilievi@outlook.com>
2023-06-20 16:58:34 +0300

    -

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,7 @@
-import "bootstrap/dist/css/bootstrap.min.css";
 import Head from 'next/head'
-import Script from 'next/script'
 import Header from "../components/header" 
 import Footer from "../components/footer" 
 import Popular from '@/components/popular_content';
-
-import "styles/App.css"
 
 export default function Home() {
   return (
@@ -13,8 +9,6 @@ export default function Home() {
     <Head>
       <title>Музей-Заповедник Кижи</title>
     </Head>
-    <Script type="text/javascript" src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossOrigin="anonymous"></Script>
-    <Script type="text/javascript" src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossOrigin="anonymous"></Script>
   
     <div className="containerImg container-fluid" >
       <Header></Header>
